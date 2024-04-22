c4e6693e9945964a59a70ce1dcf936b4a2e11c9e
Evidlo <evan@widloski.com>
2024-04-21 20:30:07 -0500

    coeffs init, catch KeyboardInterrupt

--- sph_raytracer/retrieval.py
+++ sph_raytracer/retrieval.py
@@ -15,7 +15,7 @@ def detach_loss(loss):
     """
     return float(loss.detach().cpu()) if isinstance(loss, t.Tensor) else loss
 
-def gd(f, y, model, num_iterations=100,
+def gd(f, y, model, coeffs=None, num_iterations=100,
        loss_fns=[SquareLoss()], lr=1e-1, optimizer=t.optim.Adam,
        optim_args={}, progress_bar=True
        ):
@@ -29,6 +29,8 @@ def gd(f, y, model, num_iterations=100,
         f (Forward): forward operator with pytorch autograd support
         y (tensor): measurement stack
         model (science.model.Model): initialized model
+        coeffs (tensor): initial value of coeffs before optimizing.
+            should have `requires_grad=True`.  defaults to `t.ones(model.coeffs_shape)`
         loss_fns (list[science.Loss]): custom loss functions which
             accept (f, y, density, coeffs) as args.  Losses are summed
         num_iterations (int): number of gradient descent iterations
@@ -48,12 +50,13 @@ def gd(f, y, model, num_iterations=100,
     if y is not None:
         y.requires_grad_()
 
-    coeffs = t.ones(
-        model.coeffs_shape,
-        requires_grad=True,
-        device=f.device,
-        dtype=t.float64
-    )
+    if coeffs is None:
+        coeffs = t.ones(
+            model.coeffs_shape,
+            requires_grad=True,
+            device=f.device,
+            dtype=t.float64
+        )
 
     best_loss = float('inf')
     best_coeffs = None
@@ -64,35 +67,40 @@ def gd(f, y, model, num_iterations=100,
     # initialize empty list for logging loss values each iteration
     losses = {loss_fn: [] for loss_fn in loss_fns}
     # perform requested number of iterations
-    for _ in (pbar := tqdm(range(num_iterations), disable=not progress_bar)):
-        optimizer.zero_grad()
+    try:
+        for _ in (pbar := tqdm(range(num_iterations), disable=not progress_bar)):
+            optimizer.zero_grad()
 
-        density = model(coeffs)
+            density = model(coeffs)
 
-        fidelity = regularizer = 0
-        for loss_fn in loss_fns:
-            loss = loss_fn(f, y, density, coeffs)
-            if loss_fn.use_grad:
-                if loss_fn.fidelity:
-                    fidelity += loss
-                else:
-                    regularizer += loss
-            # log the loss
-            losses[loss_fn].append(detach_loss(loss))
+            fidelity = regularizer = 0
+            for loss_fn in loss_fns:
+                loss = loss_fn(f, y, density, coeffs)
+                if loss_fn.use_grad:
+                    if loss_fn.fidelity:
+                        fidelity += loss
+                    else:
+                        regularizer += loss
+                # log the loss
+                losses[loss_fn].append(detach_loss(loss))
 
-        pbar.set_description(f'F:{fidelity:.1e} R:{regularizer:.1e}')
+            pbar.set_description(f'F:{fidelity:.1e} R:{regularizer:.1e}')
 
-        tot_loss = fidelity + regularizer
-        # save the reconstruction with the lowest loss
-        if tot_loss < best_loss:
-            best_coeffs = coeffs
+            tot_loss = fidelity + regularizer
+            # save the reconstruction with the lowest loss
+            if tot_loss < best_loss:
+                best_coeffs = coeffs
 
-        tot_loss.backward(retain_graph=True)
-        optimizer.step()
+            tot_loss.backward(retain_graph=True)
+            optimizer.step()
 
-        # do coeffs projections after gradient step
-        if hasattr(model, 'proj'):
-            coeffs.data = model.proj(coeffs)
+            # do coeffs projections after gradient step
+            if hasattr(model, 'proj'):
+                coeffs.data = model.proj(coeffs)
+
+    # allow user to stop iterations
+    except KeyboardInterrupt:
+        pass
 
         # coeffs_log.append(coeffs.detach().cpu())
     # losses['coeffs'] = coeffs_log
