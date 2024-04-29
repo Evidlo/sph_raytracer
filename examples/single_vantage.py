d39ea9a17bc82c4d41f14ccdc3c21ffac231a167
Evidlo <evan@widloski.com>
2024-04-29 18:51:15 -0500

    clean up example

--- examples/single_vantage.py
+++ examples/single_vantage.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
+# Evan Widloski - 2024-04-01
+# Demonstration of forward raytracer operator from a single vantage point
 
 import torch as t
 import matplotlib.pyplot as plt
 
 from sph_raytracer import SphericalGrid, ConeRectGeom, Operator
 
-# define volume grid and viewing geometry vantage
-vol = SphericalGrid(shape=(50, 50, 50))
+# define spherical grid and viewing geometry vantage
+grid = SphericalGrid(shape=(50, 50, 50))
 # rectilinear detector with 45° FOV (pointed at origin by default)
 geom = ConeRectGeom(
     shape=(256, 256),
@@ -16,14 +18,15 @@ geom = ConeRectGeom(
 
 # define forward operator
 # to run on CPU, use device='cpu'
-op = Operator(vol, geom, device='cuda')
+op = Operator(grid, geom, device='cuda')
 
 # generate a simple static test volume with two nested shells
 # to run on CPU, use device='cpu'
-x = t.zeros(vol.shape, device=op.device)
+x = t.zeros(grid.shape, device=op.device)
 x[-1, :, :] += 1
 x[-10, :, :] += 1
 
+# raytrace over the volume
 result = op(x)
 
 # ----- Plotting -----
@@ -37,6 +40,7 @@ ax1.set_title('Nested Shells')
 ax1.imshow(result.detach().cpu())
 
 ax2.set_title('View Geometry')
+# operator has a .plot() method which returns a matplotlib animation
 ani = op.plot(ax2)
 
 plt.show()
