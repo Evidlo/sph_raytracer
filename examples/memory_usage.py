7f69a46b1a832026efe55fbc3cc99dfddf2d5815
Evidlo <evan_copernicus@widloski.com>
2024-05-13 22:39:49 -0500

    update memory usage expression

--- examples/memory_usage.py
+++ examples/memory_usage.py
@@ -4,38 +4,42 @@
 import numpy as np
 
 # detector size
-npix1 = 50
-npix2 = 100
+npix1 = 512
+npix2 = 512
 
 # volume size
-nrad = 50
-nele = 50
-nazi = 50
+nrad = 32
+nele = 16
+nazi = 32
 
 # number of observations
-nobs = 50
-nchan = 1 # (e.g. multiple detectors in a spacecraft)
+nobs = 25
+nchan = 2 # (e.g. multiple detectors in a spacecraft)
 
-density = (nobs, nrad, nazi, nele)
+density = (1, nrad, nazi, nele)
 density_size = 8 * np.prod(density) / 1e9 # dynamic density
 
 # max number of voxels intersecting a ray
-nvox_ray = 2 * nrad + 2 * nele + nazi
+nvox_ray = 2 * (nrad + 1) + 2 * (nele + 1) + (nazi + 1)
 # indices of intersecting voxels for each ray
 indices = (nchan, nobs, npix1, npix2, nvox_ray, 3)
-indices_dtype = 8
+indices_dtype = 8 # integer dtype
 # intersection lengths of intersecting voxels for each ray
 lens = (nchan, nobs, npix1, npix2, nvox_ray)
-lens_dtype = 2
+lens_dtype = 8 # float dtype
 # volume density values along each ray
-densities = (nobs, npix1, npix2, nvox_ray)
-densities_dtype = 2
+densities = (nchan, nobs, npix1, npix2, nvox_ray)
+densities_dtype = 8 # float dtype
+# density values after sorting
+aftersort = densities
+aftersort_dtype = densities_dtype # float dtype
 
 # total peak memory usage in GB
-static_size = 2 * (
+static_size = (
     (indices_dtype * np.prod(indices)) +
     (lens_dtype * np.prod(lens)) +
-    (densities_dtype * np.prod(densities))
+    (densities_dtype * np.prod(densities)) +
+    (aftersort_dtype * np.prod(aftersort))
 ) / 1e9
 
 print('\n--- Parameters ---\n')
